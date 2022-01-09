329749d9470714bdcb6d3877aacfbbe76de261c1
Mattral <88831350+Mattral@users.noreply.github.com>
2022-01-09 09:20:02 +0630

    Add files via upload

---
+++ project.R
@@ -0,0 +1,75 @@
+library(lattice)
+library(ggplot2)
+library(caret)
+library(kernlab)
+library(rattle)
+library(corrplot)
+set.seed(1234)
+
+traincsv <- read.csv("./data/pml-training.csv")
+testcsv <- read.csv("./data/pml-testing.csv")
+
+dim(traincsv)
+
+dim(testcsv)
+
+traincsv <- traincsv[,colMeans(is.na(traincsv)) < .9] #removing mostly na columns
+traincsv <- traincsv[,-c(1:7)] #removing metadata which is irrelevant to the outcome
+
+#Removing near zero variance variables.
+nvz <- nearZeroVar(traincsv)
+traincsv <- traincsv[,-nvz]
+dim(traincsv)
+
+#split the training set into a validation and sub training set
+inTrain <- createDataPartition(y=traincsv$classe, p=0.7, list=F)
+train <- traincsv[inTrain,]
+valid <- traincsv[-inTrain,]
+
+control <- trainControl(method="cv", number=3, verboseIter=F)
+
+#Decision Tree Model
+mod_trees <- train(classe~., data=train, method="rpart", trControl = control, tuneLength = 5)
+fancyRpartPlot(mod_trees$finalModel)
+
+#Prediction
+pred_trees <- predict(mod_trees, valid)
+cmtrees <- confusionMatrix(pred_trees, factor(valid$classe))
+cmtrees
+
+#Random Forest
+mod_rf <- train(classe~., data=train, method="rf", trControl = control, tuneLength = 5)
+
+pred_rf <- predict(mod_rf, valid)
+cmrf <- confusionMatrix(pred_rf, factor(valid$classe))
+cmrf
+
+#Gradient Boosted Trees
+mod_gbm <- train(classe~., data=train, method="gbm", trControl = control, tuneLength = 5, verbose = F)
+
+pred_gbm <- predict(mod_gbm, valid)
+cmgbm <- confusionMatrix(pred_gbm, factor(valid$classe))
+cmgbm
+
+#Support Vector Machine
+mod_svm <- train(classe~., data=train, method="svmLinear", trControl = control, tuneLength = 5, verbose = F)
+
+pred_svm <- predict(mod_svm, valid)
+cmsvm <- confusionMatrix(pred_svm, factor(valid$classe))
+cmsvm
+
+#Predictions on Test Set
+pred <- predict(mod_rf, testcsv)
+print(pred)
+
+#correlation matrix of variables in training set
+corrPlot <- cor(train[, -length(names(train))])
+corrplot(corrPlot, method="color")
+
+#Plotting the models
+
+plot(mod_trees)
+
+plot(mod_rf)
+
+plot(mod_gbm)
